6d66f1ac058750fdc5f63143d94685a61f0d58a2
tj-bu <tj-bu@DESKTOP-RN83G83>
2022-07-28 17:59:32 +0900

    1

--- Gittest/src/gittest/gittest.java
+++ Gittest/src/gittest/gittest.java
@@ -4,7 +4,7 @@ public class gittest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java  123123123123");
+		System.out.println("Hello Java  12312312312113");
 		
 		
 	}
